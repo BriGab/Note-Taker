db0fc1f75dd8bbeb2c03a5de55b28fd42f2f0fe9
Bri Gab <brigabiola@outlook.com>
2020-02-10 12:06:53 -0700

    functional

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs")
+
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -9,15 +10,9 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// Notes
-// =============================================================
-var note = [
-  {
-    title: "title",
-    text: "note"
-  },
-];
+var notes = require("./db/db.json")
 
 // HTML Routes
 // =============================================================
@@ -34,14 +29,17 @@ app.get("/notes", function(req, res) {
 
 // API Routes save to db.json
 app.post("/api/notes", function(req, res) {
-var newNote = req.body;
-console.log(newNote)
-fs.appendFile(__dirname + "/db/db.json", JSON.stringify(newNote), function(error, data) {
+var id = notes.length + 1
+var newNote = {title: req.body.title, text: req.body.text, id: id}
+var completeNotes = notes.concat(newNote)
+
+fs.writeFile(__dirname + "/db/db.json", JSON.stringify(completeNotes), function(error, data) {
     if(error) {
         return error
     }
 })
-  res.json(newNote);
+  console.log(completeNotes)
+  res.json(completeNotes);
 })
 
 // Pull from db.json
@@ -50,17 +48,61 @@ app.get("/api/notes", function(req, res) {
     if (error) {
         return console.log(error)
     }
-    console.log(data)
+    console.log("This is Notes", notes)
   })
-  res.json(newNote)
+  res.json(notes)
 });
 
 app.delete("/api/notes/:id", function(req, res) {
+    const noteId = JSON.parse(req.params.id)
+    console.log(noteId)
+  
+    const filtered_list = notes.filter(val => val.id !== noteId)
 
-    var chosen = req.params.id
+    notes = filtered_list
 
-    console.log(chosen)
+    console.log(notes)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function(error, data) {
+      if(error) {
+          return error
+      }
+      res.json(notes)
+  })
 })
+
+// server.delete("/items/:id", (req, res) => {
+//   const itemId = req.params.id;
+
+//   console.log("Delete item with id: ", itemId);
+
+//   // filter list copy, by excluding item to delete
+//   const filtered_list = data.filter(item => item.id !== itemId);
+
+//   // replace old list with new one
+//   data = filtered_list;
+
+//   res.json(data);
+// });
+
+// const noteId = req.params.id
+
+//     fs.readFile(__dirname + "/db/db.json", 'utf8', function(error, data){
+//     if (error) {
+//         return console.log(error)
+//     }
+//     for (let i = 0; i < notes.length; i ++) {
+//         console.log(notes[1].id)
+//         if (notes[i].id === noteId) {
+//             deleteNote();
+//         }
+//     }
+//     console.log(notes)
+//     res.json(notes);
+
+//const deletedNote = notes.some(notes => notes.id == noteId);
+//console.log(deletedNote);
+
 
 // Starts the server to begin listening
 // =============================================================

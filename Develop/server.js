df611f62b3a5ddc9f846644eeeb0786a0eb93af2
Bri Gab <brigabiola@outlook.com>
2020-02-14 14:32:02 -0700

    final changes

--- Develop/server.js
+++ Develop/server.js
@@ -12,100 +12,74 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var notes = require("./db/db.json")
-
 // HTML Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../develop/public/index.html"));
 });
 
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 
 });
 
-// API Routes save to db.json
-app.post("/api/notes", function(req, res) {
-var id = notes.length + 1
-var newNote = {title: req.body.title, text: req.body.text, id: id}
-var completeNotes = notes.concat(newNote)
-
-fs.writeFile(__dirname + "/db/db.json", JSON.stringify(completeNotes), function(error, data) {
-    if(error) {
-        return error
+app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
     }
-})
-  console.log(completeNotes)
-  res.json(completeNotes);
+    notes = JSON.parse(notes)
+
+    var id = notes[notes.length - 1].id + 1
+    var newNote = { title: req.body.title, text: req.body.text, id: id }
+    var activeNote = notes.concat(newNote)
+
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+      if (error) {
+        return error
+      }
+      console.log(activeNote)
+      res.json(activeNote);
+    })
+  })
 })
 
 // Pull from db.json
-app.get("/api/notes", function(req, res) {
-  fs.readFile(__dirname + "/db/db.json", 'utf8', function(error, data){
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
     if (error) {
-        return console.log(error)
+      return console.log(error)
     }
-    console.log("This is Notes", notes)
+    console.log("This is Notes", data)
+    res.json(JSON.parse(data))
   })
-  res.json(notes)
 });
 
-app.delete("/api/notes/:id", function(req, res) {
-    const noteId = JSON.parse(req.params.id)
-    console.log(noteId)
-  
-    const filtered_list = notes.filter(val => val.id !== noteId)
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = JSON.parse(req.params.id)
+  console.log(noteId)
+  fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+    if (error) {
+      return console.log(error)
+    }
+    notes = JSON.parse(notes)
 
-    notes = filtered_list
+    notes = notes.filter(val => val.id !== noteId)
 
-    console.log(notes)
-
-    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function(error, data) {
-      if(error) {
-          return error
+    fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+      if (error) {
+        return error
       }
       res.json(notes)
+    })
   })
 })
-
-// server.delete("/items/:id", (req, res) => {
-//   const itemId = req.params.id;
-
-//   console.log("Delete item with id: ", itemId);
-
-//   // filter list copy, by excluding item to delete
-//   const filtered_list = data.filter(item => item.id !== itemId);
-
-//   // replace old list with new one
-//   data = filtered_list;
-
-//   res.json(data);
-// });
-
-// const noteId = req.params.id
-
-//     fs.readFile(__dirname + "/db/db.json", 'utf8', function(error, data){
-//     if (error) {
-//         return console.log(error)
-//     }
-//     for (let i = 0; i < notes.length; i ++) {
-//         console.log(notes[1].id)
-//         if (notes[i].id === noteId) {
-//             deleteNote();
-//         }
-//     }
-//     console.log(notes)
-//     res.json(notes);
-
-//const deletedNote = notes.some(notes => notes.id == noteId);
-//console.log(deletedNote);
 
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });

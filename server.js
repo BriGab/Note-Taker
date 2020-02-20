ce7abcd77e3508b63fd4d39cb3ea39260764815e
Bri Gab <brigabiola@outlook.com>
2020-02-20 13:52:16 -0700

    adjustments for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var fs = require("fs")
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
